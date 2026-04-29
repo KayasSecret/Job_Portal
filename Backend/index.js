49f4b16bc3a6ee04571b27a0bd3af9b775e49d93
KayasSecret <kayasmishra29s@gmail.com>
2026-04-29 09:10:05 +0530

    #Update

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import env from ".env";
-dotenv.config({});
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 
@@ -11,12 +11,12 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin: 'https//localhost:5173',
-    Credentials: true
+    origin: 'http://localhost:5173',
+    credentials: true
 }
 app.use(cors(corsOptions)); 
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, ()=> {
     console.log(`Server running at port ${PORT}`);
 })
