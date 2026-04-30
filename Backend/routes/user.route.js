affc540a1328618a7615a4657006a25d5daad7b1
KayasSecret <kayasmishra29s@gmail.com>
2026-04-30 11:33:37 +0530

    #Authentication

--- Backend/routes/user.route.js
+++ Backend/routes/user.route.js
@@ -1,11 +1,9 @@
 import express from "express"
 import { updateProfile } from "../controllers/user.controller";
+import isAuthenticated from "../middlewares/isAuthenticated";
 
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
-router.route("/profile/update").post(updateProfile);
-
-
-
+router.route("/profile/update").post(isAuthenticated, updateProfile);
