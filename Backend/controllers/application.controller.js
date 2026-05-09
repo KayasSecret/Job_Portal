bfa2ce46609bb8fadfb85a0010bb140480156111
KayasSecret <kayasmishra29s@gmail.com>
2026-05-09 22:36:38 +0530

    #Update_Controller

---
+++ Backend/controllers/application.controller.js
@@ -0,0 +1,110 @@
+import { application } from "express";
+import { Application } from "../models/application.model.js"
+import { Job } from "../models/job.model.js"
+
+export const applyJob = async (req, res) => {
+    try {
+        const userId = req.id;
+        const { id: jobId } = req.params.id;
+
+        if (!jobId) {
+            return res.status(404).json({
+                message: "Job id not found.",
+                success: false
+            })
+        }
+
+        // Check whether the user has already applied for this job or not.
+        const existingApplication = await Application.findOne({ job: jobId, applicant: userId })
+
+        if (existingApplication) {
+            return res.status(400).json({
+                message: "You have already applied for the job.",
+                success: false
+            })
+        }
+
+        // Check whether the job exists or not.
+        const job = await Job.findById(jobId);
+        if (!job) {
+            return res.status(404).json({
+                message: "Job not found.",
+                success: false
+            })
+        }
+
+        // For creating a new application
+        const newApplication = await Application.create({
+            job: jobId,
+            applicant: userId
+        })
+
+        job.applications.push(newApplication._id);
+        await job.save();
+        return res.status(201).json({
+            message: "Job applied successfully.",
+            success: true
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export const getAppliedJobs = async (req, res) => {
+    try {
+        const userId = req.id;
+        const application = await Application.find({ applicant: userId }).sort({ createdAt: -1 }).populate({
+            path: 'job',
+            options: { sort: { createdAt: -1 } },
+            populate: {
+                path: 'company',
+                options: { sort: { createdAt: -1 } }
+            }
+        });
+
+        if(!application) {
+            return res.status(404).json ({
+                message: "No Applications.",
+                success: true
+            })
+        }
+
+        return res.status(200).json ({
+            application,
+            success: true
+        })
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// Admin will see how many users have applied.
+export const getApplicants = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await job.findById(jobId).populate({
+            path: 'applications',
+            options: { sort: {createdAt: -1}},
+            populate: {
+                path: 'applicant'
+            }
+        });
+
+        if(!job) {
+            return res.status(404).json ({
+                message: "Job not found.",
+                success: false
+            })
+        }
+
+        return res.status(200).json ({
+            job,
+            success: true
+        })
+    }
+    catch(error) {
+        console.log(error);
+    }
+}
