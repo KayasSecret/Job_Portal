eafb7fe5ea17f0e0349ccc66908a021714f30239
KayasSecret <145342764+KayasSecret@users.noreply.github.com>
2026-05-29 19:47:29 +0530

    #Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import Navbar from './components/shared/Navbar'
+import { createBrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/auth/login'
+import Signup from './components/auth/Signup'
 import './App.css'
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/login",
+    element: <Login />
+  },
+  {
+    path: "/signup",
+    element: <Signup />
+  }
+])
 
 function App() {
   const [count, setCount] = useState(0)
--- Frontend/src/components/shared/Navbar.jsx
+++ Frontend/src/components/shared/Navbar.jsx
@@ -1,35 +1,73 @@
 import React from 'react'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
-import { Button } from "../ui/button";
+import { Avatar, AvatarImage } from '../ui/avatar'
+import { Button } from '../ui/button'
+import { LogOut, User2 } from "lucide-react";
 
 function Navbar() {
+    const user = false;
     return (
         <div className="bg-white">
             <div className="flex items-center justify-between container mx-auto max-w-7xl h-16">
                 <div>
                     <h1 className="text-2xl font-bold">Job <span className='text-[#F83002]'>Portal</span></h1>
                 </div>
-                <div>
+                <div className="flex items-center gap-12">
                     <ul className='flex font-medium items-center gap-5'>
                         <li>Home</li>
                         <li>Jobs</li>
                         <li>Browse</li>
                     </ul>
 
-                    <Popover>
-                        <PopoverTrigger asChild>
-                            <Avatar>
-                                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                                <AvatarFallback>CN</AvatarFallback>
-                            </Avatar>
-                        </PopoverTrigger>
-                        <PopoverContent>
-                            <h1>Hello </h1>
-                        </PopoverContent>
-                    </Popover>
+                    {
+                        !user ?
+                            (
+                                <div className="flex items-center gap-2">
+                                    <Button variant="outline" className="cursor-pointer">Login</Button>
+                                    <Button variant="outline" className="bg-[#6A38C2] text-white hover:bg-[#270069] hover:text-white cursor-pointer">Signup</Button>
+                                </div>
+                            ) :
+                            (
+                                <Popover>
+                                    {/* POPOVERTRIGGER */}
+                                    <PopoverTrigger asChild>
+                                        <Avatar className="cursor-pointer">
+                                            <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                        </Avatar>
+                                    </PopoverTrigger>
+
+                                    {/* POPOVERCONTENT */}
+                                    <PopoverContent className="w-80">
+                                        <div>
+                                            <div className="flex gap-4">
+                                                <Avatar className="cursor-pointer">
+                                                    <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                                                </Avatar>
+                                                <div>
+                                                    <h2 className="font-medium">Hire Nest</h2>
+                                                    <p className="text-sm text-muted-foreground"> Lorem ipsum dolor sit amet.</p>
+                                                </div>
+                                            </div>
+
+                                            <div className="flex flex-col my-2 text-600">
+                                                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                                                    <User2 />
+                                                    <Button variant="link" className="cursor-pointer">View Profile</Button>
+                                                </div>
+                                                <div className="flex w-fit items-center gap-2 cursor-pointer">
+                                                    <LogOut />
+                                                    <Button variant="link" className="cursor-pointer">Logout</Button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </PopoverContent>
+                                </Popover>
+                            )
+
+                    }
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
