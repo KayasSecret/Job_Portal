5c24aaef2512a50a0c7103ca37a5f309d7cb8cfb
KayasSecret <145342764+KayasSecret@users.noreply.github.com>
2026-06-05 15:12:17 +0530

    #Update

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -25,6 +25,10 @@ const appRouter = createBrowserRouter([
     element: <Jobs />
   }, 
   {
+    path: "/description/:id",
+    element: <JobDescription />
+  },
+  {
     path: "/browse",
     element: <Browse />
   },
--- Frontend/src/components/Profile.jsx
+++ Frontend/src/components/Profile.jsx
@@ -8,14 +8,17 @@ import { Pen } from 'lucide-react';
 import { Mail } from 'lucide-react';
 import { Contact } from 'lucide-react';
 import { Badge } from './ui/badge';
+import { Label } from './ui/label';
+import AppliedJobTable from './AppliedJobTable';
 
 const skills = ["html", "css", "js", "react", "node", "express", "mongodb", "mysql"]
 
 const Profile = () => {
+    const isResume = true;
     return (
         <div>
             <Navbar />
-            <div className="max-w-7xl mx-auto bg-white border border-gray-300 rounded-2xl my-5 p-8">
+            <div className="max-w-4xl mx-auto bg-white border border-gray-300 rounded-2xl my-5 p-8">
                 <div className="flex justify-between">
                     <div className="flex items-center gap-4">
                         <Avatar className="w-24 h-24">
@@ -41,12 +44,28 @@ const Profile = () => {
                     </div>
                 </div>
 
-                <div>
+                <div className="my-5">
                     <h1>Skills</h1>
+                    <div className="flex items-center gap-1">
+                        {
+                            skills.length !== 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>No skills added</span>
+                        }
+                    </div>
+                </div>
+
+                <div className="grid-w-full max-w-sm items-center gap-1.5">
+                    <Label className="text-md font-bold">Resume</Label>
                     {
-                        skills.length !== 0 ? skills.map((item, index) => <Badge key={index}>{item}</Badge>) : <span>No skills added</span>
+                        isResume ? <a target="blank" href="https://youtube.com/@patelmernstack" className="text-blue-500 w-full hover:underline cursor-pointer">Kayas MERN Stack</a> : <span>Not applicable</span>
                     }
                 </div>
+
+            </div>
+
+            <div className="max-w-4xl mx-auto bg-white rounded-2xl">
+                <h1 className="font-bold text-lg my-5">Applied Jobs</h1>
+                {/* Application Table */}
+                <AppliedJobTable />
             </div>
         </div>
     )
