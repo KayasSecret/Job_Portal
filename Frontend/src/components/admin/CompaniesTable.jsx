7d07b6f6f044922ec1a908f79ca10fe12373fded
KayasSecret <145342764+KayasSecret@users.noreply.github.com>
2026-06-19 21:47:37 +0530

    #Update

--- Frontend/src/components/admin/CompaniesTable.jsx
+++ Frontend/src/components/admin/CompaniesTable.jsx
@@ -8,6 +8,7 @@ import { useNavigate } from 'react-router-dom'
 
 const CompaniesTable = () => {
     const { companies, searchCompanyByText } = useSelector(store => store.company)
+    const [filterCompany, setFilterCompany] = useState(companies)
     const navigate = useNavigate()
 
     useEffect(() => {
