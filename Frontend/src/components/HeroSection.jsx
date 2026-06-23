9f54f83a0c64d954f1f8410b3341f0de5581584c
KayasSecret <145342764+KayasSecret@users.noreply.github.com>
2026-06-23 22:32:30 +0530

    #Update

--- Frontend/src/components/HeroSection.jsx
+++ Frontend/src/components/HeroSection.jsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
       <div className="flex flex-col gap-6 my-10">
         <span className="mx-auto px-4 py-2 rounded-full bg-gray-100 text-[#F83002] font-bold">Amazing Job Portal Website</span>
         <h1 className="text-5xl font-bold">Search, Apply & <br />Get Your <span className="text-[#6A38C2]">Dream Job</span></h1>
-        <p>Find the perfect job that matches your skills, passion,  experience.</p>
+        <p>Find the perfect job that matches your skills, passion, role and experience.</p>
 
         <div className="flex w-[40%] shadow-lg border border-gray-200 rounded-full pl-3 items-center mx-auto">
           <input
